2c60ac29bd7032bedbd4cbdd4ac6ebe31916bbab
Islam Ahmed <islam.ahmed-abd-alkarim@broadcom.com>
2025-04-13 09:03:59 +0300

    html-css-course

--- nodejs/AdvancedNodeCourse/config/dev.js
+++ nodejs/AdvancedNodeCourse/config/dev.js
@@ -1,6 +1,7 @@
 module.exports = {
-    googleClientID: "",
-    googleClientSecret: "",
-    mongoURI: "",
+    googleClientID: "866530191006-3emh7ob18mlf690fo0qr40k4j4i1algc.apps.googleusercontent.com",
+    googleClientSecret: "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",
+    mongoURI:
+        "mongodb+srv://admin:rXfrybBjj07jN2G9@cluster0.nixzj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     cookieKey: "123123123",
 };
